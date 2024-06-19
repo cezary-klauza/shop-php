264fe0f1c44d0b764ed62e00538b469a78b38795
cezary-klauza <83139353+cezary-klauza@users.noreply.github.com>
2024-06-19 11:27:24 +0200

    Added cart items counter

--- nav.php
+++ nav.php
@@ -1,22 +1,33 @@
 <?php
-$cart = json_decode($_COOKIE['cart']);
+$cart;
 $total_price = 0.00;
-foreach ($cart as $key => $value) {
-    $id = intval($value);
-    $sql_query = "SELECT name, price FROM products WHERE id={$id}";
-    $res = mysqli_query($conn, $sql_query);
-    $r = mysqli_fetch_assoc($res);
-    $price = floatval($r['price']);
-    $total_price = $total_price + $price;
+$cart_count = 0;
+if (isset($_COOKIE['cart'])) {
+    $cart = json_decode($_COOKIE['cart']);
+    foreach ($cart as $key => $value) {
+        $cart_count++;
+        $id = intval($value);
+        $sql_query = "SELECT name, price FROM products WHERE id={$id}";
+        $res = mysqli_query($conn, $sql_query);
+        $r = mysqli_fetch_assoc($res);
+        $price = floatval($r['price']);
+        $total_price = $total_price + $price;
+    }
 }
 ?>
 <link rel="stylesheet" href="style/nav.css">
 <nav>
-    <div id="nav-conteiner">
+    <div id="nav-container">
         <h1><a href="index.php">logo</a></h1>
-        <div id="cart-conteiner">
-            <p><?php echo $total_price; ?>$</p>
+        <div id="nav-cart-container">
+            <p class="cart-price"><?php echo $total_price; ?>$</p>
             <a href="cart.php"><img src="./assets/cart.svg" alt="cart"></a>
+            <?php
+            if ($cart_count > 0) { ?>
+                <div class="cart-count">
+                    <p><?php echo $cart_count; ?></p>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </nav>
